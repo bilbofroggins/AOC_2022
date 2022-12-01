4025d49f8e2e31ef3ba9ef36264ea7af9fd8a1d4
Pat Cunniff <pat.cunniff@drizly.com>
2022-12-01 10:26:12 -0500

    day1

--- day1.py
+++ day1.py
@@ -1,1 +1,36 @@
-test
+def part1():
+    calories = [0]
+
+    with open('day1_input.txt') as f:
+        elf = 0
+        for line in f:
+            val = line.strip()
+            if val == "":
+                elf += 1
+                calories.append(0)
+                continue
+
+            calories[elf] += int(val)
+
+    calories.sort(reverse=True)
+    print(calories[0])
+
+def part2():
+    calories = [0]
+
+    with open('day1_input.txt') as f:
+        elf = 0
+        for line in f:
+            val = line.strip()
+            if val == "":
+                elf += 1
+                calories.append(0)
+                continue
+
+            calories[elf] += int(val)
+
+    calories.sort(reverse=True)
+    print(calories[0] + calories[1] + calories[2])
+
+part1()
+part2()
